7f4761750877dacbb63841639cf6987bc409b71d
mohamedmubaader <135695067+mohamedmubaader@users.noreply.github.com>
2024-02-18 09:48:00 +0300

    New commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from '@/components/ui/button';
-import { SelectTrigger, SelectValue, Select, SelectContent, SelectItem } from '@/components/ui/select';
-import { Label } from "@/components/ui/label"
+//import { SelectTrigger, SelectValue, Select, SelectContent, SelectItem } from '@/components/ui/select';
+//import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 
 
@@ -32,9 +32,7 @@ export default function Home() {
       productName: "",
       quantity: "",
       price: "",
-   
-
-      
+  
     }
   });
 
